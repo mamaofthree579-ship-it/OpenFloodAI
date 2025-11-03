0dcf1f60f560c422289cd9a7e4abc95ade388296
mamaofthree579-ship-it <mamaofthree579@gmail.com>
2025-11-02 22:07:18 -0500

    Update make_baseline_map.py

--- make_baseline_map.py
+++ make_baseline_map.py
@@ -119,7 +119,6 @@ ax.set_title("Sacramento - Verona Baseline Environmental Map (Offline)")
 ax.axis('off')
 plt.legend()
 
-# save and show
-plt.savefig(out_png, dpi=300, bbox_inches='tight')
-plt.show()
-print("✅ Offline map complete. Saved:", out_gpkg, out_png)
+# show
+plt.show(out_png, dpi=300, bbox_inches='tight')
+print("✅ Offline map complete. Saved:", out_gpkg)
